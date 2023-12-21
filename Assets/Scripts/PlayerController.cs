d9a8d03a471591c7a809acac70fa5ef9b17e0e64
Dennis Hackethal <dh@dennishackethal.com>
2023-12-20 17:02:23 -0700

    Make player jump up at start of game

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,10 +4,19 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private Rigidbody playerRb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        // This can also be achieved by making playerRb public
+        // and dragging the rigidbody component into the corresponding
+        // field in the Unity editor. Then the line below
+        // can be removed.
+        playerRb = GetComponent<Rigidbody>();
+
+        // Make player jump up
+        playerRb.AddForce(Vector3.up * 1000);
     }
 
     // Update is called once per frame
