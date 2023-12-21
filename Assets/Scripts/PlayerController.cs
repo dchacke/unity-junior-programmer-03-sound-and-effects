1beccadbfc2c3c32b78d40041e9b4aadbe7593d2
Dennis Hackethal <dh@dennishackethal.com>
2023-12-21 14:39:31 -0700

    Set up a falling animation
    I also sped up the animation a bit as it looked too slow for my taste.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -46,6 +46,8 @@ public class PlayerController : MonoBehaviour
         }
         else if (collision.gameObject.CompareTag("Obstacle"))
         {
+            playerAnim.SetBool("Death_b", true);
+            playerAnim.SetInteger("DeathType_int", 1);
             gameOver = true;
             Debug.Log("Game over!");
         }
